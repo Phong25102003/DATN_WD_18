7abed7a643fd9ce5c99286988cc8fdb7e582d6b8
quanglk <quanglkph26960@fpt.edu.vn>
2024-02-14 15:04:00 +0700

    LuongQuang_Ph26960

---
+++ server/models/Color.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+const dataSchema = new mongoose.Schema({}, { collection: "Color", timestamps: true, strict: false });
+
+dataSchema.set('toJSON', {
+    virtuals: true,
+    versionKey: false,
+    transform: function (doc, ret) {
+      ret.id = ret._id;
+      delete ret._id;
+    }
+});
+
+const Color = mongoose.model('Color', dataSchema);
+
+module.exports = Color;
