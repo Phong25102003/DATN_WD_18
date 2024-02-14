63dabecba6f72432728c71f123f236f646324f58
quangnv963 <nguyenquang090603@gmail.com>
2024-02-14 11:48:47 +0700

    add Orders and Product_incoming

---
+++ server/models/Product_incoming.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+const dataSchema = new mongoose.Schema({}, { collection: "Product_Incoming", timestamps: true, strict: false });
+
+dataSchema.set('toJSON', {
+    virtuals: true,
+    versionKey: false,
+    transform: function (doc, ret) {
+      ret.id = ret._id;
+      delete ret._id;
+    }
+});
+
+const Product_Incoming = mongoose.model('Product_Incoming', dataSchema);
+
+module.exports = Product_Incoming;
